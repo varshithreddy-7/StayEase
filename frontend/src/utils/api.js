0b0a0bf4f124755607d1f7a59baa00f0ec542227
varshithreddy-7 <varshithreddymandala07@gmail.com>
2026-04-11 20:51:03 +0530

    final fix hotels page

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: (process.env.REACT_APP_API_URL || 'http://localhost:5000') + '/api',
+  baseURL: 'https://stayease-backend-5wim.onrender.com/api',
   withCredentials: true,
 });
+
+
 
 // Attach token to every request
 API.interceptors.request.use((config) => {
